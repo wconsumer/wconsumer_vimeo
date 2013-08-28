c7cd1e2bc4e8485f336e8aea07b67abaab99f177
kcgzerone <kcgzerone@gmail.com>
2013-08-28 21:40:39 +0800

    2nd commit

--- wconsumer_vimeo.js
+++ wconsumer_vimeo.js
@@ -1,3 +1,35 @@
+
 (function ($) {
-    
+    Drupal.behaviors.wconsumer_vimeoEditForm = {
+        attach: function (context, settings) {
+            $('.wconsumer-vimeo-widget-upload-type', context).click(function() {
+                $('.wconsumer-vimeo-widget-form').hide();
+                $(this).next().slideDown('slow');
+                
+                var instance = $(this);
+                $('.wconsumer-vimeo-widget-upload-form input[type="hidden"]', context).each(function() {
+                    var name = $(this).attr('name');
+                    if (name && name.indexOf('[upload_type]') >= 0)
+                        $(this).val(instance.attr('data-type'));
+                });
+            });
+            
+            $('.wconsumer-vimeo-widget .wconsumer-vimeo-widget-existing-videos ul li').click(function() {
+                $('.wconsumer-vimeo-widget .wconsumer-vimeo-widget-existing-videos ul li').removeClass('selected');
+                $(this).addClass('selected');
+                
+                var instance = $(this);
+                $('.wconsumer-vimeo-widget-upload-form input[type="hidden"]', context).each(function() {
+                    var name = $(this).attr('name');
+                    if (name && name.indexOf('[vvid]') >= 0)
+                        $(this).val(instance.attr('data-video-id'));
+                });
+            });
+        }
+    };
+
+    Drupal.behaviors.wconsumer_vimeoViewForm = {
+        attach: function (context) {
+        }
+    };
 })(jQuery);
